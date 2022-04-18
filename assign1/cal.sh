e88bbffce316bedd8364e364f5ca86c09a604c3b
Ubuntu <ubuntu@ip-172-31-0-208.us-east-2.compute.internal>
2022-04-18 11:20:17 +0000

    update

--- assign1/cal.sh
+++ assign1/cal.sh
@@ -1,19 +1,19 @@
 $x 
 $y
 $op
-if [ $op == 1 ]
+if [[ $op == 1 ]]
 then
 a=`expr $x + $y`
 echo $a
-elif [ $op == 2 ]
+elif [[ $op == 2 ]]
 then
 b=`expr $a - $b`
 echo $b
-elif [ $op == 3 ]
+elif [[ $op == 3 ]]
 then
 c=`expr $x \* $y`
 echo $c
-elif [ $op == 4 ]
+elif [[ $op == 4 ]]
 then
 d=`expr $x / $y`
 echo $d
