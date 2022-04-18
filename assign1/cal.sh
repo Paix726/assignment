441e344074b9559bb514ca2f6cc5e2caa0c9c7e5
Ubuntu <ubuntu@ip-172-31-0-208.us-east-2.compute.internal>
2022-04-18 11:10:53 +0000

    update

---
+++ assign1/cal.sh
@@ -0,0 +1,21 @@
+$x 
+$y
+$op
+if [ $op == 1 ]
+then
+a=`expr $x + $y`
+echo $a
+elif [ $op == 2 ]
+then
+b=`expr $a - $b`
+echo $b
+elif [ $op == 3 ]
+then
+c=`expr $x \* $y`
+echo $c
+elif [ $op == 4 ]
+then
+d=`expr $x / $y`
+echo $d
+fi
+
